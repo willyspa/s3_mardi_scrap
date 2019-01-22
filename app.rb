14842b03a616e1306271ff054b9fc9fdb9192011
willyspa <armed95@hotmail.fr>
2019-01-22 23:24:32 +0100

    retirer coms

--- app.rb
+++ app.rb
@@ -7,38 +7,7 @@ require 'views/index'
 require 'views/done'
 #require 'views/fichier_2'
 
-index = Index.new
-index.choix
+Index.new.choix
+
 
 Done.new.recommencer
-
-
-=begin
-puts "Quel format de fichier pour les donées scrappé ?"
-puts "taper 1 pour JSON"
-puts "taper 2 pour google spreadsheet"
-puts "taper 3 pour CSV"
-
-user_choice = gets.chomp
-
-if user_choice == "1"
-
-    scrap = Scrapping.new
-    scrap.save_as_json
-
-elsif user_choice == "2"
-
-    scrap = Scrapping.new
-    scrap.save_as_spreadsheet
-
-elsif user_choice == "3"
-
-  scrap = Scrapping.new
-  scrap.save_as_csv
-
-else
-
-  puts "recommencer"
-
-end
-=end
